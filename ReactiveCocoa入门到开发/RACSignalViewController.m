a822c8cf42116d2f05708f8728450b6f18b5ae31
zp <haozp@haozpdeMacBook-Pro.local>
2017-05-11 09:36:04 +0800

    RACSignal说明

--- ReactiveCocoa入门到开发/RACSignalViewController.m
+++ ReactiveCocoa入门到开发/RACSignalViewController.m
@@ -24,33 +24,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"创建信号");
+    //RACSignal使用步骤：
+    // 1.创建信号 + (RACSignal *)createSignal:(RACDisposable * (^)(id<RACSubscriber> subscriber))didSubscribe
+    // 2.订阅信号,才会激活信号. - (RACDisposable *)subscribeNext:(void (^)(id x))nextBlock
+    // 3.发送信号 - (void)sendNext:(id)value
     
+    
+    // RACSignal底层实现：
+    // 1.创建信号，首先把didSubscribe保存到信号中，还不会触发。
+    // 2.当信号被订阅，也就是调用signal的subscribeNext:nextBlock
+    // 2.2 subscribeNext内部会创建订阅者subscriber，并且把nextBlock保存到subscriber中。
+    // 2.1 subscribeNext内部会调用siganl的didSubscribe
+    // 3.siganl的didSubscribe中调用[subscriber sendNext:@1];
+    // 3.1 sendNext底层其实就是执行subscriber的nextBlock
+    
+    //创建信号
     RACSignal *signal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {//subscriber 订阅者
 
-        NSLog(@"发送信号");
+        //发送信号
         [subscriber sendNext:@"hello RAC!"];
-        
-        
+        [subscriber sendCompleted];
+
+        //取消订阅
+        // block调用时刻：当信号发送完成或者发送错误，就会自动执行这个block,取消订阅信号。
+        // 执行完Block后，当前信号就不在被订阅了。
+        // 如果不在发送数据，最好发送信号完成，内部会自动调用[RACDisposable disposable]取消订阅信号。
+        //[subscriber sendCompleted];
         return [RACDisposable disposableWithBlock:^{
-            //取消信号 1:信号发送完 2:信号发送失败
+            
             NSLog(@"取消订阅");
         }];
     }];
     
+    //订阅信号
     [signal subscribeNext:^(id x) {
-        NSLog(@"订阅信号");
+        
         NSLog(@"%@",x);
     }];
     
     
     
-//    //手动取消订阅
-//    RACDisposable *disposable = [signal subscribeNext:^(id  _Nullable x) {
-//        NSLog(@"%@",x);
-//    }];
-//    //手动取消订阅
-//    [disposable dispose];
+    //订阅信号
+    RACDisposable *disposable = [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    //手动取消订阅
+    [disposable dispose];
 
 }
 
